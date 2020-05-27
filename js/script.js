3b1d35add6621aaf28f5a6058a65f94712dfc3a8
bipin <bipin@studiomatrix.com.np>
2020-05-27 12:04:06 +0545

    applying final js touches

--- js/script.js
+++ js/script.js
@@ -8,12 +8,23 @@ const listTitleElement = document.querySelector('[data-list-title]');
 const listCountElement = document.querySelector('[data-list-count]');
 const taskContainer = document.querySelector('[data-tasks]');
 const taskTemplate = document.getElementById('task-template');
+const newTaskForm = document.querySelector('[data-new-task-form]');
+const newTaskInput  = document.querySelector('[data-new-task-input]');
+const clearCompleteTasksButton = document.querySelector('[data-clear-complete-task-button]');
+
+clearCompleteTasksButton.addEventListener('click', e => {
+    const selectedList = lists.find(list => list.id === selectedListId);
+    selectedList.tasks = selectedList.tasks.filter(task => !task.complete);
+    saveAndRender();
+})
+
 
 
 const LOCAL_STORAGE_LIST_KEY = 'task.lists';
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId';
 let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [];
 let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY);
+
 listsContainer.addEventListener('click', e => {
     if (e.target.tagName.toLowerCase() === 'li') {
         selectedListId = e.target.dataset.listId;
@@ -21,6 +32,15 @@ listsContainer.addEventListener('click', e => {
     }
 })
 
+taskContainer.addEventListener('click', e => {
+if (e.target.tagName.toLowerCase() === 'input') {
+    const selectedList = lists.find(list => list.id === selectedListId);
+    const selectedTask = selectedList.tasks.find(task => task.id === e.target.id)
+    selectedTask.complete = e.target.checked;
+    save();
+    renderTaskCount(selectedList);
+}
+})
 newListForm.addEventListener('submit', e=> {
     e.preventDefault();
     const listName = newListInput.value;
@@ -28,6 +48,17 @@ newListForm.addEventListener('submit', e=> {
     const list = createList(listName);
     newListInput.value = null;
     lists.push(list);
+    saveAndRender();
+})
+
+newTaskForm.addEventListener('submit', e=> {
+    e.preventDefault();
+    const taskName = newTaskInput.value;
+    if (taskName == null || taskName === '') return;
+    const task = createTask(taskName);
+    newTaskInput.value = null;
+    const selectedList = lists.find(list => list.id === selectedListId );
+    selectedList.tasks.push(task);
     saveAndRender();
 })
 deleteListBtn.addEventListener('click', e => {
@@ -53,15 +84,24 @@ function render() {
         listDisplayContainer.style.display = '';
         listTitleElement.innerText = selectedList.name;
         renderTaskCount(selectedList);
-        clearElement(tasksContainer);
+        clearElement(taskContainer);
         renderTasks(selectedList);
 
     }
 }
-function renderTasks(list) {
-
+function renderTasks(selectedList) {
+    selectedList.tasks.forEach(task => {
+    const taskElement = document.importNode(taskTemplate.content, true);
+    const checkbox = taskElement.querySelector('input');
+    checkbox.id = task.id;
+    checkbox.checked = task.complete;
+    const label = taskElement.querySelector('label');
+    label.htmlFor = task.id;
+    label.append(task.name);
+    taskContainer.appendChild(taskElement);
+})
 }
-function renderTaskCount(list) {
+function renderTaskCount(selectedList) {
     const incompleteTasksCount = selectedList.tasks.filter(task => !task.complete).length;
     const taskString = incompleteTasksCount === 1? "task": "tasks";
     listCountElement.innerText = `${incompleteTasksCount} ${taskString} remaining`;
@@ -77,7 +117,14 @@ function renderList() {
     });
 }
 function createList(name) {
-    return {id: Date.now().toString(), name: name, tasks: []}
+    return {id: Date.now().toString(), name: name, tasks: [{
+            id: 'sda',
+            name: 'Test',
+            complete: true
+        }]}
+}
+function createTask(name) {
+    return {id: Date.now().toString(), name: name, complete: false}
 }
 function clearElement(element) {
         while(element.firstChild) {
